15233d8b2d06c1ee9575b96e00a2d1d137acffa2
Mefistofel <mfstfl@gmail.com>
2023-08-21 22:10:40 +0200

    ignore

--- html/castle.js
+++ html/castle.js
@@ -1,8 +1,31 @@
-function Castle(x, y) {
+function Castle(x, y, factionId = -1) {
 	var castle = {
-		position: [x, y],
-		life: 0,
-		max: 10
+		pos: [x, y],
+		faction: factionId,
+		lives: 10,
+		max: 100,
+		draw(canvas) {
+			const castleHeight = 60;
+			canvas.fillStyle = getFactionColor(castle.faction);  // white
+			canvas.fillRect (castle.pos[0] - castleHeight, castle.pos[1] - castleHeight, castleHeight + castleHeight, castleHeight + castleHeight)
+			canvas.fillText(castle.lives, castle.pos[0], castle.pos[1] - 70);
+		},
+		apply() {
+			if (castle.lives < castle.max) {
+				castle.lives += 1;
+			}
+		}
 	}
 	return castle;
+}
+
+function getFactionColor(factionId) {
+	switch (factionId) {
+		case 0: return '#0000FF'; // player
+		case 1: return '#FF0000'; // enemy
+		case 2: return '#00FF00'; // enemy 2
+		case -1:
+		default:	
+			return '#555555'; // neutral
+	}
 }
